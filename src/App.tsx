5169e04752821140fc9e5ee9d7f983123db1b783
Pranjal Shikhar Sinha <shikhar.pranjal3@gmail.com>
2024-11-24 17:23:16 +0530

    app routes + layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from "react";
-import { Button } from "./components/ui/button";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout";
+import { ThemeProvider } from "./context/theme-provider";
+import CityPage from "./pages/city-page";
+import WeatherDashboard from "./pages/weather-dashboard";
 
 const App = () => {
   return (
-    <div>
-      <Button variant="destructive">Click Me</Button>
-    </div>
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="dark">
+        <Layout>
+          <Routes>
+            <Route path="/" element={<WeatherDashboard />} />
+            <Route path="/city/:cityName" element={<CityPage />} />
+          </Routes>
+        </Layout>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 };
 
